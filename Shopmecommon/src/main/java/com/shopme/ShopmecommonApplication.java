03ccd0b0ddd52960530c86c2aca2bcfbf73b7f9b
sakthi-14492 <sakthivel.km@zohocorp.com>
2023-10-15 13:40:04 +0530

    Added Shopmeproject

---
+++ Shopmecommon/src/main/java/com/shopme/ShopmecommonApplication.java
@@ -0,0 +1,13 @@
+package com.shopme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmecommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmecommonApplication.class, args);
+	}
+
+}
---
+++ Shopmewebparent/src/main/java/com/shopme/ShopmewebparentApplication.java
@@ -0,0 +1,13 @@
+package com.shopme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmewebparentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmewebparentApplication.class, args);
+	}
+
+}
